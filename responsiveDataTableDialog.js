5b30aaf32323e58f31b4b71ca2ae92be96ffbc28
briapete <brian.peterson@kineticdata.com>
2015-06-02 08:18:34 -0500

    Added configuration

--- responsiveDataTableDialog.js
+++ responsiveDataTableDialog.js
@@ -1,47 +1,42 @@
 
 
 $(document).ready(function() {
+	//Append Slide Panel Dive
 	$( ".content-slide" ).after( "<div id='slide-panel'></div>" );
 	
+	//Bind Events to Search Elements
 	$('#name_search input').keypress(function(e) {
     if(e.which == 13) {
         $(this).siblings().find('i').removeClass('fa-search').addClass('fa-spinner fa-pulse');
-		performBridgeRequest(peopleTables.requestedForTableConfig);
+		peopleTables.requestedForTableConfig.execute();
+		//performBridgeRequest(peopleTables.requestedForTableConfig);
     }
 	});
 	$('#name_search a').click(function(e) {
          $(this).find('i').removeClass('fa-search').addClass('fa-spinner fa-pulse');
-		performBridgeRequest(peopleTables.requestedForTableConfig);
+		peopleTables.requestedForTableConfig.execute();
+	});
+
+
+	$('#contact_search input').keypress(function(e) {
+    if(e.which == 13) {
+        $(this).siblings().find('i').removeClass('fa-search').addClass('fa-spinner fa-pulse');
+		peopleTables.contactTableConfig.execute();
+    }
+	});
+	$('#contact_search a').click(function(e) {
+         $(this).find('i').removeClass('fa-search').addClass('fa-spinner fa-pulse');
+		peopleTables.contactTableConfig.execute();
 	});
 	
-	var timer;
-	$('#name_search input').keyup(function() {
-		if (timer) {
-			clearTimeout(timer);
-		}
-		//timer = setTimeout("alert($('#name_search input').val());", 500);
-		timer = setTimeout("performBridgeRequestList(peopleTables.requestedForTableConfig2);", 500);
-			//performBridgeRequest2(peopleTables.requestedForTableConfig);
-		//	$(this).siblings().find('i').removeClass('fa-search').addClass('fa-spinner fa-pulse');
-
-	});
-
-
-$('#Search').keypress(function () { 
-    
-});
-	/***************** CONFIGURE: the Fields to be used in the table *****************
-	//This Array of objects defines searching and behavior for Simple Data Requests (SDRs).
-	//Each Object in the array defines a search and its behavior and must be created with a
-	//configuration that works with a simple data request.
-	/*********************************************************************************/
+	//Define defaults for the Table config.  Reduces need to include all properties.  Each table config my overide these values by 
+	//including a value of its own.
 	var tableDefaults = {
 		initiateSearch: function (SRID){defaultSearch(this, SRID)},
 		//rowclick: function (){defaultRowCallback(this);},
 		paging: true,
 		info: true,
 		searching: true,
-		//responsive: true,
 		responsive: {
 			details: {
 				type: 'column',
@@ -51,13 +46,28 @@ $('#Search').keypress(function () {
 			}
 		},
 	};
+	
+	//Define defaults for the List config.  Reduces need to include all properties.  Each list config my overide these values by 
+	//including a value of its own.
 	var listDefaults = {
 
 	};
 	
+	// Define Table objects or list Object
+	// TODO:Possibly separate Tables from List is separate JS or config
 	peopleTables = {
 		requestedForTableConfig: {
+			execute: performBridgeRequest,
+			//type: "DataTable" or "List"
 			type: "DataTable",
+			bridgeConfig:{
+				model: "Person",
+				qualification_mapping: "By First Name or Last Name or Full Name",
+				//Params to be created and passed to the Bridge.  VALUE MUST BE JQUERY SELECTOR.
+				parameters: {'Full Name': '#name_search input','First Name': '#name_search input','Last Name': '#name_search input'},
+				//CONFIGURE: Bridge Attributes to be returned
+				attributes: ["First Name","Last Name","Email","Login Id","Work Phone Number"],
+			},
 			//Where to append the table
 			appendTo: $('#slide-panel'),
 			//ID to give the table when creating it.
@@ -66,25 +76,23 @@ $('#Search').keypress(function () {
 			afterInit: function(){ //completeCallback
 				
 			},
+			// After Table Load
 			loadedcallback: function(){
+				//Show Panel
 				var panel = $('#slide-panel');
-				//if (!panel.hasClass("visible")) {
 					panel.addClass('visible').animate({
 						'right': '0'
 					});
-				//}
-				//KD.utils.Action.insertElement('RequestedForTable');
-				$('#'+this.tableId + ' tr td:first-child').on('click',function(){
+				//Bind clice event to Panel
+				//TODO: Can this be moved to make callback more intuitive
+				$('#'+this.tableId ).on( "click", 'tr td:first-child', function(){
 					defaultRowCallback(this);
 					$('input#name_search').val(KD.utils.Action.getQuestionValue('ReqFor_First Name')+ ' ' + KD.utils.Action.getQuestionValue('ReqFor_Last Name'));
-					//KD.utils.Action.removeElement('RequestedForTable');
 					$('#name_search a').find('i').removeClass('fa-spinner fa-pulse').addClass('fa-search');
 					var panel = $('#slide-panel');
-					//if (panel.hasClass("visible")) {
 						panel.removeClass('visible').animate({
 							'right': '-100%'
 						});
-					//} 
 				});
 			},
 			/*Configure the data to be displayed in the table and set into Question Values
@@ -93,6 +101,7 @@ $('#Search').keypress(function () {
 			//data: Must match the data returned by the Simple Data Request.
 			//title: Display name used in the table header.
 			//setQstn: Question Element to be set with the selected value. (Only when defaultRowCallback is used)
+			//className: Used with DataTables Responsive Plugin.
 			*/
 			columns: [
 				{ data: 'select', "title":"SELECT",	orderable: false, width: 60,			className: "select" },
@@ -115,11 +124,86 @@ $('#Search').keypress(function () {
 				KD.utils.Action.setQuestionValue('Search By Last Name','');	
 				$( "#dialog" ).dialog("open");
 			},
+			//TODO: Not sure if this is currently leveraged
+			noResults: function(){
+				alert("No results Found")
+			}
+		},
+		contactTableConfig: {
+			execute: performBridgeRequest,
+			//type: "DataTable" or "List"
+			type: "DataTable",
+			bridgeConfig:{
+				model: "Person",
+				qualification_mapping: "By First Name or Last Name or Full Name",
+				//Params to be created and passed to the Bridge.  VALUE MUST BE JQUERY SELECTOR.
+				parameters: {'Full Name': '#contact_search input','First Name': '#contact_search input','Last Name': '#contact_search input'},
+				//CONFIGURE: Bridge Attributes to be returned
+				attributes: ["First Name","Last Name","Email","Login Id","Work Phone Number"],
+			},
+			//Where to append the table
+			appendTo: $('#slide-panel'),
+			//ID to give the table when creating it.
+			tableId: 'contactTable',
+			//After the Table has been created.
+			afterInit: function(){ //completeCallback
+				
+			},
+			// After Table Load
+			loadedcallback: function(){
+				//Show Panel
+				var panel = $('#slide-panel');
+					panel.addClass('visible').animate({
+						'right': '0'
+					});
+				//Bind clice event to Panel
+				//TODO: Can this be moved to make callback more intuitive
+				$('#'+this.tableId ).on( "click", 'tr td:first-child', function(){
+					defaultRowCallback(this);
+					$('input#contact_search').val(KD.utils.Action.getQuestionValue('Contact_First Name')+ ' ' + KD.utils.Action.getQuestionValue('Contact_Last Name'));
+					$('#contact_search a').find('i').removeClass('fa-spinner fa-pulse').addClass('fa-search');
+					var panel = $('#slide-panel');
+						panel.removeClass('visible').animate({
+							'right': '-100%'
+						});
+				});
+			},
+			/*Configure the data to be displayed in the table and set into Question Values
+			//This is a modified object used by Datatables.net.  setQstn has been added to it.
+			//columns: Array of Objects.
+			//data: Must match the data returned by the Simple Data Request.
+			//title: Display name used in the table header.
+			//setQstn: Question Element to be set with the selected value. (Only when defaultRowCallback is used)
+			//className: Used with DataTables Responsive Plugin.
+			*/
+			columns: [
+				{ data: 'select', "title":"SELECT",	orderable: false, width: 60,			className: "select" },
+				{ data: 'First Name', "title":"FIRST", "setQstn":"Contact_First Name",		className: "control" },
+				{ data: 'Last Name', "title":"LAST", "setQstn":"Contact_Last Name",			className: "min-phone-l control-additional" },
+				{ data: 'Email', "title":"EMAIL", "setQstn":"Contact_Email",					className: "min-tablet control-additional" },
+				{ data: 'Login Id', "title":"LOGIN ID", "setQstn":"Contact_Login ID",		className: "none " },
+				{ data: 'Work Phone Number', "title":"PHONE #", "setQstn":"Contact_Phone",	className: "none" },
+			],
+			//Params to be created and passed to the Simple Data Request (SDR).  SDR may be created to accept parameters
+			//which can be passed into the query. Passed into SDR as: "lname=Bob&Status=Active" 
+			params: {
+				lname: function() {
+					return  ucFirst(KD.utils.Action.getQuestionValue('Search By Last Name'));
+				},
+				status: "Active"
+			},
+			//Define action to take place after SDR is complete.
+			done: function (){
+				KD.utils.Action.setQuestionValue('Search By Last Name','');	
+				$( "#dialog" ).dialog("open");
+			},
+			//TODO: Not sure if this is currently leveraged
 			noResults: function(){
 				alert("No results Found")
 			}
 		},
 		requestedForTableConfig2: {
+			execute: performBridgeRequestList,
 			type: "List",
 			bridgeConfig:{
 				model: "Person",
@@ -131,8 +215,6 @@ $('#Search').keypress(function () {
 					},
 			//Where to append the table
 			appendTo: $('#DYNAMIC_TEXT_KSHAA5V0HJEMVANOR2RUDKJ9A318PZ'),
-			//ID to give the table when creating it.
-			tableId: 'requestedForTable',
 			//After the Table has been created.
 			afterInit: function(){ //completeCallback
 				
@@ -165,13 +247,12 @@ $('#Search').keypress(function () {
 			}
 		}
 	}
-
+	//Initialize the configurations
 	$.each(peopleTables, function(i, config){
 		if(config.type=="DataTable"){
 			config=$.extend( {}, tableDefaults, config );
-			$table =($('<table>', {'cellspacing':'0', 'border':'0', 'class': 'display'})).attr('id',config.tableId);
+			$table =($('<table>', {'cellspacing':'0', 'border':'0', 'class': 'display'})).attr('id',config.tableId).data('table-config',i);
 			config.appendTo.append($table);
-			//config.afterInit();
 			peopleTables[i].tableObj = $('#'+config.tableId).DataTable( config );
 			config.afterInit();
 		}
@@ -180,52 +261,28 @@ $('#Search').keypress(function () {
 			config.appendTo.append("<div id='results'>");
 			config.afterInit();
 		}
-		/* Legacy code - Save for a bit longer
-		peopleTables[i]=$.extend( {}, tableDefaults, peopleTables[i] );
-		$table =($('<table>', {'cellspacing':'0', 'border':'0', 'class': 'display'})).attr('id',peopleTables[i].tableId);
-		peopleTables[i].appendTo.append($table);
-		//peopleTables[i].afterInit();
-		peopleTables[i].tableObj = $('#'+peopleTables[i].tableId).DataTable( peopleTables[i] );
-		peopleTables[i].afterInit();
-		
-		//peopleTables[i].rowclick();
-		//peopleTables[i].tableObj.on( 'click', 'tr', peopleTables[i].rowclick);
-		//peopleTables[i].tableObj.on( 'click', peopleTables[i].rowclick);
-		*/
-		});
+	});
 } );
 
 function defaultRowCallback(row){ //rowCallback
 	var id = $(row).closest('table').attr('id');
+var tableConfig = $(row).closest('table').data('tableConfig')
 	selectedRow = $(row).closest('tr');
-	table = filterByTableId (peopleTables, id);
+	table = peopleTables[tableConfig]
 	
-	$.each(table[0].columns, function( j, v){
+	$.each(table.columns, function( j, v){
 		if(v["setQstn"]!="" && typeof v["setQstn"] != "undefined"){
-			KD.utils.Action.setQuestionValue(v["setQstn"],table[0].tableObj.row(selectedRow).data()[v["data"]]);
-			//                                      $('#'+table[0].tableId).DataTable().row($(row).closest('tr')).data()
+			KD.utils.Action.setQuestionValue(v["setQstn"],table.tableObj.row(selectedRow).data()[v["data"]]);
 		}
 	});
 	$(".ui-dialog-content").dialog("close");
 }
-/* Previous Version of Default Row callback
-function defaultRowCallback(row){ //rowCallback
-	var id = $(row).closest('table').attr('id');
-	var selectedRow = $(row).index();
-	table = filterByTableId (peopleTables, id);
-	
-	$.each(table[0].columns, function( j, v){
-		if(v["setQstn"]!="" && typeof v["setQstn"] != "undefined"){
-			KD.utils.Action.setQuestionValue(v["setQstn"],table[0].tableObj.row( row ).data()[v["data"]]);
-		}
-	});
-	$(".ui-dialog-content").dialog("close");
-}
-*/
+
 function defaultSearch(tableConfig, SDRId){  
 	var params = buildParams(tableConfig);
 	performSDR(tableConfig, SDRId, params);
 }
+
 function buildParams(tableConfig){
 	tableConfig.fullParams = "";
 	$.each(tableConfig.params, function( j, v ){
@@ -239,18 +296,20 @@ function buildParams(tableConfig){
 	return tableConfig.fullParams;
 }
 
-function performBridgeRequest(tableConfig){
+function performBridgeRequest(){
+	var tableConfig = this;
+	//Retrieve and set the Bridge parameter values using JQuery
+	var parameters = {};
+	$.each(tableConfig.bridgeConfig.parameters, function(i,v){
+		parameters[i]=$(tableConfig.bridgeConfig.parameters[i]).val();
+	});
 	//create the connector necessary to connect to the bridge
 	var connector = new KD.bridges.BridgeConnector();
 	//CONFIGURE: Id of table (div) to recieve Bridge results.  The table element must exist before this code executes.
-	var tableId = peopleTables.requestedForTableConfig.tableId;
-	//CONFIGURE: Define Bridge and Model/qualification to use
-	connector.search('Person', 'By First or Last Name', {
-		//CONFIGURE: Bridge Parameters
-		parameters: {'Last Name': $('#name_search input').val(),'First Name': $('#name_search input').val()},
-		//CONFIGURE: Bridge Attributes to be returned
-		attributes: ["First Name","Last Name","Email","Login Id","Work Phone Number"],
-		//CONFIGURE: Function to process results after search
+	var tableId = this.tableId;
+		connector.search(tableConfig.bridgeConfig.model, tableConfig.bridgeConfig.qualification_mapping, {
+		parameters: parameters,
+		attributes: tableConfig.bridgeConfig.attributes,
 		success: function(response) {
 			tableConfig.dataArray = [];
 			//Retrieve Records
@@ -291,7 +350,9 @@ function performBridgeRequest(tableConfig){
 		}
 	);      
 }
-function performBridgeRequestList(tableConfig){
+
+function performBridgeRequestList(){
+	var tableConfig = this;
 	//Retrieve and set the Bridge parameter values using JQuery
 	var parameters = {};
 	$.each(tableConfig.bridgeConfig.parameters, function(i,v){
@@ -334,6 +395,7 @@ function performBridgeRequestList(tableConfig){
 		}
 	);      
 }
+
 function performSDR(tableConfig, SDRId, params){
 	//	Define a callback that will call the custom populateUserTable function on success, or alert on failure. 
 	var connection=new KD.utils.Callback(function(response){
