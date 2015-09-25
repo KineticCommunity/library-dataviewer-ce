c5c75e1985b0767783b962918586c7f7f9c54005
briapete <brian.peterson@kineticdata.com>
2015-09-25 09:48:11 -0500

    account for templateId ==null

--- search.js
+++ search.js
@@ -130,7 +130,7 @@
 		$.each(configObj.bridgeConfig.parameters, function(i,v){
 			parameters[i]=$(configObj.bridgeConfig.parameters[i]).val();
 		});
-		var templateId = (configObj.bridgeConfig.templateId) ? configObj.bridgeConfig.templateId : clientManager.templateId;
+		var templateId = (configObj.bridgeConfig.templateId && configObj.bridgeConfig.templateId!="null") ? configObj.bridgeConfig.templateId : clientManager.templateId;
 		//create the connector necessary to connect to the bridge
 		var connector = new KD.bridges.BridgeConnector({templateId: templateId});
 		//CONFIGURE: Id of table (div) to recieve Bridge results.  The table element must exist before this code executes.
@@ -194,7 +194,7 @@
 		$.each(configObj.bridgeConfig.parameters, function(i,v){
 			parameters[i]=$(configObj.bridgeConfig.parameters[i]).val();
 		});
-		var templateId = (configObj.bridgeConfig.templateId) ? configObj.bridgeConfig.templateId : clientManager.templateId;
+		var templateId = (configObj.bridgeConfig.templateId && configObj.bridgeConfig.templateId!="null") ? configObj.bridgeConfig.templateId : clientManager.templateId;
 		//create the connector necessary to connect to the bridge
 		var connector = new KD.bridges.BridgeConnector({templateId: templateId});
 		//CONFIGURE: Id of table (div) to recieve Bridge results.  The table element must exist before this code executes.
@@ -241,7 +241,7 @@
 		$.each(configObj.bridgeConfig.parameters, function(i,v){
 			parameters[i]=$(configObj.bridgeConfig.parameters[i]).val();
 		});
-		var templateId = (configObj.bridgeConfig.templateId) ? configObj.bridgeConfig.templateId : clientManager.templateId;
+		var templateId = (configObj.bridgeConfig.templateId && configObj.bridgeConfig.templateId!="null") ? configObj.bridgeConfig.templateId : clientManager.templateId;
 		//create the connector necessary to connect to the bridge
 		var connector = new KD.bridges.BridgeConnector({templateId: templateId});
 		connector.search(configObj.bridgeConfig.model, configObj.bridgeConfig.qualification_mapping, {
